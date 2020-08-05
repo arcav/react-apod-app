110954128b9440a00a7ca794713f95e55acb73f5
arcav <armandojcastro84@gmail.com>
2020-08-05 11:57:04 -0400

    produccion

---
+++ build/precache-manifest.bd580c4255180f478bf258ad13e2f207.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fedab8190180fcbb95cd4d09a098dc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab22454ce4892d32345",
+    "url": "/static/css/main.27b5ba17.chunk.css"
+  },
+  {
+    "revision": "1cee66aa436a62be0722",
+    "url": "/static/js/2.e346af71.chunk.js"
+  },
+  {
+    "revision": "fab22454ce4892d32345",
+    "url": "/static/js/main.899d4e18.chunk.js"
+  },
+  {
+    "revision": "0289fdfa5d08eef01ab4",
+    "url": "/static/js/runtime-main.afc08828.js"
+  },
+  {
+    "revision": "6a6df70d0a12bf31341f3479f9e678ee",
+    "url": "/static/media/apodBackground.6a6df70d.png"
+  },
+  {
+    "revision": "9d97c8ffe64b90d9af642dba03a2ef5c",
+    "url": "/static/media/nasa.9d97c8ff.png"
+  },
+  {
+    "revision": "80687d23d8eef70e255e1996a06d9f5c",
+    "url": "/static/media/react1.80687d23.png"
+  }
+]);
