d91c2702992061d36c05b69331accb31b3c39644
arcav <armandojcastro84@gmail.com>
2021-08-12 01:09:26 -0400

    styled-components and dependencies update

--- src/CustomsHooks/useFetch.js
+++ src/CustomsHooks/useFetch.js
@@ -2,30 +2,27 @@
 
 import { useState, useEffect } from "react";
 
-
 //Custom Hook For Fetch
 
 function useFetch(url) {
-	const [data, setData] = useState(url);
-	
-	useEffect(() => {
-		getData();
-		async function getData() {
-			try {
-				const response = await fetch(url);
-				const data = await response.json();
-				setData(data);
-			}
-			catch (e) {
-				console.error(e);
-			}
-		}
-		
-	}, [url]);
-	
-	if (!data) return null;
-	
-	return data;
+    const [data, setData] = useState(url);
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                setData(data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        getData();
+    }, [url]);
+
+    if (!data) return null;
+
+    return data;
 }
 
 export default useFetch;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,14 +1,14 @@
 //React and React-Router-Dom
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
 //Component Pages
-import Home from '../Pages/Home';
-import Apod from '../Pages/Apod';
-import About from '../Pages/About';
-
+import Home from "../Pages/Home";
+import Apod from "../Pages/Apod";
+import About from "../Pages/About";
 
 //Routes of App
+
 const Routes = () => {
     return (
         <>
