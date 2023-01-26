45396b7cb93233127c1fca82eeb6a54d443e7fc2
arcav <armandojcastro84@gmail.com>
2023-01-25 21:35:48 -0400

    deploy fix

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -27,35 +27,6 @@ export const About = () => {
       </div>
      
     </div>
-   /*  <div className="h-screen bg-red-500">
-      <div className="h-screen flex flex-col">
-        <div className="bg-green-500">
-          <h1 className="text-6xl text-white font-bold text-center  ">
-            {" "}
-            A.P.O.D{" "}
-          </h1>
-          <h3 className="text-2xl text-white mt-8 font-bold text-center">
-            Astronomy Picture of the Day
-          </h3>
-        </div>
-
-        <p className="text-xl text-white font-semibold mt-8 p-4">
-          {" "}
-          One of the most popular websites at NASA is the Astronomy Picture of
-          the Day. In fact, this website is one of the most popular websites
-          across all federal agencies. It has the popular appeal of a Justin
-          Bieber video.This endpoint structures the APOD imagery and associated
-          metadata so that it can be repurposed for other applications.In
-          addition,f the concept_tags parameter is set to True, then keywords
-          derived from the image explanation are returned. These keywords could
-          be used as auto - generated hashtags for twitter or instagram feeds;
-          but generally help with discoverability of relevant imagery.
-        </p>
-      </div>
-    </div> */
-  );
+   );
 };
 
-{
-  /*  */
-}
