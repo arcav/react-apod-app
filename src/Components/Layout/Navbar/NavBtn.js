5f9411b68f180b1cb59a3e06dc3aff0cda4686aa
arcav <armandojcastro84@gmail.com>
2023-01-26 19:22:18 -0400

    fix:NavBtn change hover and shadow color

--- src/Components/Layout/Navbar/NavBtn.js
+++ src/Components/Layout/Navbar/NavBtn.js
@@ -5,8 +5,8 @@ export const NavBtn = ({ children, path }) => {
   return (
     <NavLink to={path}>
       <div
-        className="bg-red-700 w-11/12 h-12 border border-solid  border-yellow-200 
-      hover:bg-yellow-200 hover:text-red-500 hover:border-red-500 hover:shadow-md hover:shadow-yellow-200 m-auto flex items-center justify-center"
+        className="bg-red-700 w-11/12 h-12 border border-solid shadow-md shadow-white border-yellow-200
+      hover:bg-white hover:text-red-500 hover:border-red-500 hover:shadow-md hover:shadow-red-500 m-auto flex items-center justify-center"
       >
         {children}
       </div>
