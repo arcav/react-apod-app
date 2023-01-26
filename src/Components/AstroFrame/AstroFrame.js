684f1400602a7caf52d8f33577e2fa2ac8e76caf
arcav <armandojcastro84@gmail.com>
2023-01-26 16:41:17 -0400

    feature:Astroframe Component

---
+++ src/Components/AstroFrame/AstroFrame.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const AstroFrame = ({ children, dataUrl }) => {
+  return (
+    <iframe
+      start="true"
+      className="rounded-3xl my-2"
+      title="space-video"
+      src={dataUrl}
+      allow="autoplay"
+      allowFullScreen
+    />
+  );
+};
