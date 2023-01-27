1930681690584df178ff74c18f505e58137175da
arcav <armandojcastro84@gmail.com>
2023-01-27 17:07:06 -0400

    fix:About Page height responsive for each screen and margins

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Content = ({ children }) => {
   return (
-    <p className="text-xl text-white font-semibold mt-8 p-4"> {children}</p>
+    <p className="text-xl text-white font-semibold p-4 mb-6"> {children}</p>
   );
 };
