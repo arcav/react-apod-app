acd1a4a0b8cfa958547392987c7f78e61741dced
arcav <armandojcastro84@gmail.com>
2023-01-27 17:12:15 -0400

    fix:Subtitle margins and paddings

--- src/Components/SubTitle/SubTitle.js
+++ src/Components/SubTitle/SubTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SubTitle = ({ children }) => {
   return (
-    <h3 className="text-2xl text-white mt-8 font-bold text-center">
+    <h3 className="text-2xl text-white p-4 font-bold text-center">
      {children}
     </h3>
   );
