a756588451bf0aa6f73f42a3806cc813f4e44290
arcav <armandojcastro84@gmail.com>
2023-01-29 15:42:08 -0400

    fix:footer algin tex center

--- src/Components/Layout/Footer/Footer.js
+++ src/Components/Layout/Footer/Footer.js
@@ -4,7 +4,9 @@ import React from "react";
 const footer = () => {
   return (
     <footer className="bg-[#010f24] flex text-lg text-white justify-center bottom-0  w-full ">
-      <div className="h-12">&copy; {new Date().getFullYear()} Copyright: arc@v</div>
+      <div className="h-12 flex items-center font-bold">
+        &copy; {new Date().getFullYear()} Copyright: arc@v
+      </div>
     </footer>
   );
 };
