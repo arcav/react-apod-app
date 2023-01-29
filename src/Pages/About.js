8a52e613a957868f5bca102c64daab654db80862
arcav <armandojcastro84@gmail.com>
2023-01-29 15:41:40 -0400

    fix:small devices responsive footer position

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,7 +8,7 @@ import { PageName } from "../Components/PageName/PageName";
 import { SubTitle } from "../Components/SubTitle/SubTitle";
 export const About = () => {
   return (
-    <div className="flex flex-col h-full justify-items-start m-auto w-11/12 md:h-screen">
+    <div className="flex flex-col h-screen justify-items-start m-auto w-11/12 md:h-screen">
       <div className="mt-10">
         <PageName>APOD</PageName>
       </div>
